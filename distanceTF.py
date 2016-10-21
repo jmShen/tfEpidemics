1b08ac4fc5b6c20b324296564e40cb2fbff19b24
jmShen <j.shen65535@outlook.com>
2016-10-21 16:26:02 +0100

    First edition of distance matrix code finished

--- distanceTF.py
+++ distanceTF.py
@@ -1,6 +1,37 @@
-import sys
-print(sys.version)
-import numpy
-import tensorflow
+import numpy as np
+import tensorflow as tf
 
-print(tensorflow.__version__)
+def tfDistance(coordinate):
+    '''
+
+    :param coordinate: is the x,y coordinate for each individual
+    :return: the distance matrix, the i-j entry of the matrix is the distance between individual i and j
+    '''
+    x=tf.constant(coordinate)
+    #DistanceMatrix=-2 * np.dot(X, X.T) + np.sum(np.square( X), 1).reshape(num_people, 1) + np.sum(np.square( X), 1).reshape(1, num_people)
+    #This is the numpy version distance matrix calculation
+    #now we want transfer it into tensorflow version
+    r=tf.reduce_sum(x*x,1)
+    r=tf.reshape(r,[-1,1])
+    D=-2*tf.matmul(x,tf.transpose(x))+r+tf.transpose(r)
+    return tf.sqrt(D)
+
+def tfDistanceLowMemory(coordinate):
+    #from Chris. Jewell
+    x = tf.constant(coordinate)
+    D=tf.map_fn(lambda i: tf.reduce_sum(tf.sqrt(tf.reduce_sum(tf.pow(i-x,2),1))),x)
+    return D
+
+def Distance(Coordinate):
+    lmModel = tfDistance(coordinate)
+    # lmModel=tfDistanceLowMemory(coordinate)
+    init = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init)
+        lmModel = sess.run(lmModel)
+        print (lmModel - cr.DistanceMatrix(coordinate))
+
+if __name__ == "__main__":
+    import coordinate as cr
+    coordinate=cr.geodata(100)
+    Distance(coordinate)
